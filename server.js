ec2391aff7a5d03f5e0950b9518be8b3ccc621eb
Fernando Severino <fernandosevjipa@gmail.com>
2022-04-14 16:23:37 -0300

    test 3

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 var app = express();
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, "../client/build", "index.html");
-  if (!url.startsWith("/app/"))
-    // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
