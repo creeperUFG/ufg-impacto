76ec79bf7046834c983b06d5675d762587e8c5d9
Fernando Severino <fernandosevjipa@gmail.com>
2022-04-14 16:15:21 -0300

    test prod

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const { resolve } = require("path");
 
-const app = express();
+const port = process.env.PORT || 8080;
+var app = express();
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+// List of all the files that should be served as-is
+let protected = ["transformed.js", "main.css", "favicon.ico"];
 
-app.listen(process.env.PORT || 3000, (err) => {
-  if (err) console.log(err);
+app.get("*", (req, res) => {
+  let path = req.params["0"].substring(1);
+
+  if (protected.includes(path)) {
+    // Return the actual file
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    // Otherwise, redirect to /build/index.html
+    res.sendFile(`${__dirname}/build/index.html`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
