b72986a90b996724e5538143d65065953178f6b4
Fernando Severino <fernandosevjipa@gmail.com>
2022-04-14 16:37:05 -0300

    fix server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { resolve } = require("path");
 const app = express();
 
 app.use("/", express.static(resolve(__dirname, "./build")));
+app.get("*", (req, res) => res.sendFile(resolve("build", "index.html")));
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) console.log(err);
